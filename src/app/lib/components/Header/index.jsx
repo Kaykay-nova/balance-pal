5e00ae026123f0306f2ab07d231eba96b5343260
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-06-09 12:11:56 +0200

    úprava stylů kvíz a aktivní navigace v hlavičce

--- src/app/lib/components/Header/index.jsx
+++ src/app/lib/components/Header/index.jsx
@@ -1,20 +1,24 @@
+"use client"
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 import '../../../style.css';
 
 export function Header() {
+  const pathname = usePathname();
+
   return (
     <header className="header">
       <div><Link className="header__logo" href="/">
       BalancePal
         </Link></div>
       <nav className="header__nav">
-        <Link className="header__link header__link--active" href="/">
+        <Link href="/" className={`header__link ${pathname === '/' ? 'header__link--active' : ''}`}>
           Domů
         </Link>
-        <Link className="header__link" href="/blog">
+        <Link href="/blog" className={`header__link ${pathname === '/blog' || pathname.includes("posts") ? 'header__link--active' : ''}`}>
           Blog
         </Link>
-        <Link className="header__link" href="/contact">
+        <Link href="/contact" className={`header__link ${pathname.includes("contact") ? 'header__link--active' : ''}`}>
           Kontakt
         </Link>
       </nav>
