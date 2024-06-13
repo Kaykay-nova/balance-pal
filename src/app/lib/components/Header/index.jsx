08a854a2cafa0069e08e78b7000dbabd467596c1
Patricie Vyhlídalová <patina.vy@gmail.com>
2024-06-13 19:42:23 +0200

    responzivita stránek Blog + článek

--- src/app/lib/components/Header/index.jsx
+++ src/app/lib/components/Header/index.jsx
@@ -1,6 +1,6 @@
-"use client"
+'use client';
 import Link from 'next/link';
-import {usePathname} from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import '../../../style.css';
 
 export function Header() {
@@ -8,17 +8,36 @@ export function Header() {
 
   return (
     <header className="header">
-      <div><Link className="header__logo" href="/">
-      BalancePal
-        </Link></div>
+      <div>
+        <Link className="header__logo" href="/">
+          BalancePal
+        </Link>
+      </div>
       <nav className="header__nav">
-        <Link href="/" className={`header__link ${pathname === '/' ? 'header__link--active' : ''}`}>
+        <Link
+          href="/"
+          className={`header__link ${
+            pathname === '/' ? 'header__link--active' : ''
+          }`}
+        >
           Domů
         </Link>
-        <Link href="/blog" className={`header__link ${pathname === '/blog' || pathname.includes("posts") ? 'header__link--active' : ''}`}>
+        <Link
+          href="/blog"
+          className={`header__link ${
+            pathname === '/blog' || pathname.includes('posts')
+              ? 'header__link--active'
+              : ''
+          }`}
+        >
           Blog
         </Link>
-        <Link href="/contact" className={`header__link ${pathname.includes("contact") ? 'header__link--active' : ''}`}>
+        <Link
+          href="/contact"
+          className={`header__link ${
+            pathname.includes('contact') ? 'header__link--active' : ''
+          }`}
+        >
           Kontakt
         </Link>
       </nav>
