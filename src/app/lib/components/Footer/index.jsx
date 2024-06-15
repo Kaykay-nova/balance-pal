9f93e9bb7220c76829ee8026e9bd5ea1d0c52efc
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-06-15 15:46:47 +0200

    Úprava burger menu a oddělení header a footer stylů ke komponentám

--- src/app/lib/components/Footer/index.jsx
+++ src/app/lib/components/Footer/index.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import '../../../style.css';
+import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
--- src/app/lib/components/Header/index.jsx
+++ src/app/lib/components/Header/index.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import '../../../style.css';
+import './style.css';
 import { useState } from 'react';
 
 export function Header() {
