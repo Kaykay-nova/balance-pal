9bf4a017ac8bcb31bc072377699a81fa8e056c99
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-06-16 21:35:32 +0200

    Upravené otázky ke kvízu a přidána fuknce pro výběr dvou možností u poslední otázky. Zároveň znovupřidání textu ke close tlačítku

--- src/app/lib/components/Quiz/index.jsx
+++ src/app/lib/components/Quiz/index.jsx
@@ -2,37 +2,46 @@
 import { useState, forwardRef } from 'react';
 import questions from "./data.json" with { type: "json" };
 import { QuizResult } from '@/app/lib/components/QuizResult';
-import './style.css'
+import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-
-export const Quiz = forwardRef( ({ onQuit, posts }, ref) => {
+export const Quiz = forwardRef(({ onQuit, posts }, ref) => {
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState([]);
   const [answers, setAnswers] = useState([]);
   const [quizEnded, setQuizEnded] = useState(false);
 
   const handleChoice = (choice) => {
-    setSelectedOption(choice);
+    if (questionNumber === questions.length) {
+      if (selectedOptions.includes(choice)) {
+        setSelectedOptions(selectedOptions.filter(opt => opt !== choice));
+      } else {
+        if (selectedOptions.length < 2) {
+          setSelectedOptions([...selectedOptions, choice]);
+        }
+      }
+    } else {
+      setSelectedOptions([choice]);
+    }
   };
 
   const handleNext = () => {
-    if (selectedOption) {
-      setAnswers([...answers, selectedOption.category]);
+    if (selectedOptions.length > 0) {
+      setAnswers([...answers, selectedOptions.map(opt => opt.category)]);
     }
     if (questionNumber === questions.length) {
       setQuizEnded(true);
     } else {
       setQuestionNumber(questionNumber + 1);
-      setSelectedOption(null);
+      setSelectedOptions([]);
     }
   };
 
   const handlePrevious = () => {
-    if (questionNumber > 0) {
+    if (questionNumber > 1) {
       setQuestionNumber(questionNumber - 1);
-      setSelectedOption(null);
+      setSelectedOptions([]);
     }
   };
 
@@ -44,89 +53,87 @@ export const Quiz = forwardRef( ({ onQuit, posts }, ref) => {
 
   const handleRestart = () => {
     setQuestionNumber(1);
-    setSelectedOption(null);
+    setSelectedOptions([]);
     setAnswers([]);
     setQuizEnded(false);
   };
 
-  
-
   return (
     <main>
       <div className="quiz__container">
-
-        <form ref={ref} className='quiz__form'>
+        <form ref={ref} className="quiz__form">
           <div className="quiz__header">
-          <div className="quiz__progressbar">
-          {!quizEnded ? 
-            <div className="quiz__counter">{questionNumber}/{questions.length}</div>  
-            : <div/>}
+            <div className="quiz__progressbar">
+              {!quizEnded ? (
+                <div className="quiz__counter">{questionNumber}/{questions.length}</div>
+              ) : <div />}
+              {!quizEnded ? (
+                <div id="progress-bar">
+                  <div id="bar" style={{ width: `${(questionNumber / questions.length) * 100}%` }} />
+                </div>
+              ) : <div />}
+            </div>
 
-            {!quizEnded ? 
-            <div id="progress-bar">
-              <div id="bar" style={{ width: `${(questionNumber / questions.length) * 100}%` }}>
-              </div>
-            </div> 
-            : <div/>}
-          </div>
-            
             <button type="button" className="quiz__cancel-btn" onClick={handleQuit}>
               <FontAwesomeIcon
                 className="close__icon"
                 icon={faXmark}
-              /></button>
+              /><span className="btn__text">{quizEnded ? 'Zavřít' : 'Ukončit'}</span>
+            </button>
           </div>
 
           {!quizEnded ? (
-            <div className='quiz__body'>
-              <h2 className='quiz__question-title'>{questions[questionNumber - 1].title}</h2>
-              <div className='quiz__questions'>
+            <div className="quiz__body">
+              <h2 className="quiz__question-title">{questions[questionNumber - 1].title}</h2>
+              <div className="quiz__questions">
                 {questions[questionNumber - 1].options.map((option, index) => (
-                  <label key={index} className={selectedOption === option ? 'quiz__question selected__btn' : 'quiz__question'}
+                  <label
+                    key={index}
+                    className={selectedOptions.includes(option) ? 'quiz__question selected__btn' : 'quiz__question'}
                   >
                     <input
-                      type={selectedOption === option ? 'hidden' : 'radio'} 
-                      name='quiz_option'
-                      checked={selectedOption === option}
+                      type={selectedOptions.includes(option) ? 'hidden' : 'radio'}
+                      name="quiz_option"
+                      checked={selectedOptions.includes(option)}
                       onChange={() => handleChoice(option)}
-                    /> 
+                    />
                     {option.text}
                   </label>
                 ))}
               </div>
             </div>
           ) : (
-
-            <QuizResult posts={posts} onRestart={handleRestart} answers={answers}/>
+            <QuizResult posts={posts} onRestart={handleRestart} answers={answers} />
           )}
 
           {!quizEnded && (
             <div className="post__buttons">
-              {questionNumber !== 1 ? <button 
-                type="button" 
-                className="post__buttons--btn" 
-                onClick={handlePrevious} 
-              >
-               &#10094; <span className='btn__text'>Předchozí</span>
-              </button> : <div/>}
-              
-              {questionNumber === questions.length ? (
-                <button 
-                  type="button" 
-                  className="post__buttons--btn" 
-                  onClick={handleNext} 
-                  disabled={!selectedOption}
+              {questionNumber !== 1 ? (
+                <button
+                  type="button"
+                  className="post__buttons--btn"
+                  onClick={handlePrevious}
                 >
-                  <span className='btn__text'>Odeslat</span> &#10149;
+                  &#10094; <span className="btn__text">Předchozí</span>
+                </button>
+              ) : <div />}
+              {questionNumber === questions.length ? (
+                <button
+                  type="button"
+                  className="post__buttons--btn"
+                  onClick={handleNext}
+                  disabled={selectedOptions.length === 0}
+                >
+                  <span className="btn__text">Odeslat</span> &#10149;
                 </button>
               ) : (
-                <button 
-                  type="button" 
-                  className="post__buttons--btn" 
-                  onClick={handleNext} 
-                  disabled={!selectedOption}
+                <button
+                  type="button"
+                  className="post__buttons--btn"
+                  onClick={handleNext}
+                  disabled={selectedOptions.length === 0}
                 >
-                  <span className='btn__text'>Další</span> &#10095;
+                  <span className="btn__text">Další</span> &#10095;
                 </button>
               )}
             </div>
--- src/app/lib/components/QuizResult/index.jsx
+++ src/app/lib/components/QuizResult/index.jsx
@@ -1,13 +1,13 @@
-'use client'
+'use client';
 import resultTexts from "./results.json" with { type: "json" };
 import Link from 'next/link';
-import './style.css'
+import './style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faRotate } from '@fortawesome/free-solid-svg-icons';
 import lottie from 'lottie-web';
 import { useEffect, useRef } from 'react';
 
-export const QuizResult = ({onRestart, answers, posts}) => {
+export const QuizResult = ({ onRestart, answers, posts }) => {
   const animationConfetti = useRef(null);
 
   useEffect(() => {
@@ -26,18 +26,18 @@ export const QuizResult = ({onRestart, answers, posts}) => {
     const counts = categories.map(category => answers.filter(answer => answer === category).length);
     const maxCount = Math.max(...counts);
     const mostChoosenCategory = categories[counts.indexOf(maxCount)];
-  
+
     return mostChoosenCategory;
   };
 
   const result = resultTexts[calculateResult()];
   const resultPosts = posts.filter((post) => {
-    return result.references.includes(post.slug)
+    return result.references.includes(post.slug);
   });
 
   const openInNewTab = (url) => {
     window.open(url, '_blank');
-  }
+  };
 
   return (
     <div className='quiz__body'>
@@ -45,20 +45,20 @@ export const QuizResult = ({onRestart, answers, posts}) => {
       <h2 className='quiz__title'>Váš výsledek</h2>
       <p className='quiz__result'>{result.text}</p>
       <ul className="quiz__result-links">
-        {resultPosts.map((p)=> {
+        {resultPosts.map((p) => {
           return (
             <li className="quiz__result-link" key={p.slug}>
               <a onClick={() => openInNewTab(`/blog/${p.slug}`)}>{p.data.title}</a>
             </li>
-          )
+          );
         })}
       </ul>
       <div className='quiz__result-btn'>
-        <button type="button" onClick={() => onRestart()} className="result-btn">
+        <button type="button" onClick={onRestart} className="result-btn">
           <FontAwesomeIcon
             className="refresh__icon"
             icon={faRotate}
-          /> 
+          />
           Znovu
         </button>
         <Link href="/blog" className="result-btn">Všechny články
@@ -69,5 +69,5 @@ export const QuizResult = ({onRestart, answers, posts}) => {
         </Link>
       </div>
     </div>
-  )
-}
+  );
+};
