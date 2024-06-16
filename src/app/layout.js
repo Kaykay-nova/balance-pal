5b19df302326aca43f2c7699efc7789d5e32b0a7
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-06-16 02:02:02 +0200

    Přidána favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,35 @@
 import { Inter } from 'next/font/google';
 import { Header } from '@/app/lib/components/Header';
 import { Footer } from '@/app/lib/components/Footer';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'BalancePal',
   description: 'Vrátíme vám vaši ztracenou rovnováhu',
-};
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="cs">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon-16x16.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+      </Head>
       <body className={inter.className}>
         <div className='page'>
-        <Header />
-        {children}
-        <Footer />
+          <Header />
+          {children}
+          <Footer />
         </div>
       </body>
     </html>
